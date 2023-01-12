613d987cee9a0a0cf9399600c575b0bac4e1c504
amylianne <amywynn.3.a@gmail.com>
2023-01-12 08:41:06 +0000

    finished MVP

--- dealership.js
+++ dealership.js
@@ -5,18 +5,58 @@ const Dealership = function(name, maxNumberCars, carsInStock){
     this.maxNumberCars = maxNumberCars;
     this.carsInStock = carsInStock;
 }
+
+
 //METHODS
-// 1. Count the number of cars in stock
+// 1. Count the number of cars in stock-PASSES
 Dealership.prototype.countCarsInStock = function (){
     return this.carsInStock.length;
 }
 
-// 2. Add a car to stock
+// 2. Add a car to stock-PASSES
 Dealership.prototype.addCar = function (newCar){
     return this.carsInStock.push(newCar);
 }
-// 3. Return an array containing each car's manufacturer
-// 4. Find all the cars from a given manufacturer
-// 5. Find the total value of all the cars in stock
+// 3. Return an array containing each car's manufacturer-PASSES
+Dealership.prototype.findManufacturers = function (){
+    //MAPPING
+    const carManu = this.carsInStock.map((carsInStock) => carsInStock.manufacturer);
+    //remove duplicate values in the array
+    let uniqueManu = [...new Set(carManu)];
+    return uniqueManu;
+}
+
+//// 4. Find all the cars from a given manufacturer- DOESN'T PASS
+// //input a manufacturer and return cars array
+// Dealership.prototype.carsFromGivenManu = function(manufacturer){
+// //check if car's manufacturer (Car[0]) matches given manufacturer 
+// //loop through Car's in carsInStock 
+// const carsInStock = this.carsInStock.map((carsInStock) => carsInStock)
+// //get only manufacturer then match back with rest of
+// const [manu, ... restOfArray] = Dealership.carsInStock;
+
+//     if (this.Car[0]== manufacturer){
+//         return carsInStock = manu + restOfArray;
+//     }
+// }
+
+
+
+// 5. Find the total value of all the cars in stock -PASSES
+//create new prototype function
+Dealership.prototype.sumOfCars = function (){
+
+//get array of car prices from carsInStock, using MAPPING:
+const carPrices = this.carsInStock.map((carsInStock) => carsInStock.price)
+
+//add together carPrices using REDUCING:
+const sumOfCars = carPrices.reduce((accumulator, carPrices) => {
+    return accumulator + carPrices;
+},0)
+return sumOfCars;
+}
+
+
+
 
 module.exports = Dealership;
--- dealership.test.js
+++ dealership.test.js
@@ -2,57 +2,57 @@ const Dealership = require('./dealership');
 //import car class so can create new cars
 const Car = require('./car');
 
+//add some dealership variables
+let name = "Amy's dealership";
+let maxNumberCars = 50;
+let carsInStock = [new Car("Toyota", 20000,"Type-A"), new Car("Ford", 24300,"Type-A"), new Car("Nissan", 2300,"Type-B"),new Car("Nissan", 400,"Type-C")]
+// create a new dealerhsip
+beforeEach( () => {
+    dealership = new Dealership(name, maxNumberCars, carsInStock);
+})
+
 //TEST 1
 test('can count number of cars in stock in the dealership', () => {
-//add somes cars and a new dealerhsip
-    let name = "Amy's dealership";
-    let maxNumberCars = 50;
-    let carsInStock = [new Car("Toyota", 20000,"Type-A"), new Car("Ford", 24300,"Type-A"), new Car("Nissan", 2300,"Type-B")]
-    let dealership = new Dealership(name, maxNumberCars, carsInStock)
-//compare
-    const expected = 3;
+  //compare carsInStock
+    const expected = 4;
     const actual = dealership.countCarsInStock();
     expect(actual).toBe(expected);
 });
 //TEST 2
 
 test('can add car to the dealership stock', () => {
-    let name = "Amy's dealership";
-    let maxNumberCars = 50;
-    let carsInStock = [new Car("Toyota", 20000,"Type-A"), new Car("Ford", 24300,"Type-A"), new Car("Nissan", 2300,"Type-B")]
-    let dealership = new Dealership(name, maxNumberCars, carsInStock)
     let newCar = new Car("Porsche", 90000,"3.7 L 6-cylinder")
     //add newCar 
     dealership.addCar(newCar);
     //compare number of cars in stock now
-    const expected = 4
+    const expected = 5;
     const actual = dealership.countCarsInStock();
     expect(actual).toBe(expected);
 });
 //TEST 3
 
-test.skip('can return an array containing each cars manufacturer', () => {
-
+test('can return an array containing each cars manufacturer', () => {
+//from  carsInStock get frist argument (manufacterer) of Car. 
     //compare
-    const expected = 3;
-    const actual = dealership.countCarsInStock();
-    expect(actual).toBe(expected);
+    const expected = ["Toyota","Ford","Nissan","Porsche"];
+    const actual = dealership.findManufacturers();
+    expect(actual).toStrictEqual(expected);
 });
+//not sure why toBe didn't work, only toStrictEqual works???
+
 //TEST 4
 
 test.skip('can find all the cars from a given manufacturer', () => {
-
-    //compare
-    const expected = 3;
-    const actual = dealership.countCarsInStock();
+    const expected =  [("Nissan", 2300,"Type-B"), ("Nissan", 400,"Type-C")]
+    const actual = dealership.carsFromGivenManu('Nissan');
     expect(actual).toBe(expected);
 });
 //TEST 5
 
-test.skip('can find the total value of all the cars in stock', () => {
+test('can find the total value of all the cars in stock', () => {
 
     //compare
-    const expected = 3;
-    const actual = dealership.countCarsInStock();
+    const expected = 137000;
+    const actual = dealership.sumOfCars();
     expect(actual).toBe(expected);
 });
